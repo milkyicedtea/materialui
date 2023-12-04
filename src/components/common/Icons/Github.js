277a7adad3a56643ac09f4883b88e515c4fdcd93
Ignorance <81954613+milkyicedtea@users.noreply.github.com>
2023-12-04 09:42:58 +0100

    fallback image not working :sob:

--- src/components/common/Icons/Github.js
+++ src/components/common/Icons/Github.js
@@ -1,16 +1,18 @@
 import Box from "@mui/material/Box";
-import React from "react";
+import React, {useState} from "react";
 import {LinksProperties} from "./IconsProperties";
-
 
 const Github = ({sx, alt}) => {
     return (
         <Box
             component="img"
             sx={sx}
-            alt={(alt) ? alt : 'Github Profile'}
-            src={LinksProperties['Github'].src}
+            alt={(alt) || 'Github Profile'}
+            src={LinksProperties['Github']?.src}
             onClick={() => LinksProperties['Github'].handleClick()}
+            onError={(e) => {
+                e.target.src = './img-github-mark-white.png'
+            }}
         />
     )
 }
--- src/components/common/Icons/IconsProperties.js
+++ src/components/common/Icons/IconsProperties.js
@@ -11,27 +11,39 @@ export const LinksProperties = {
         src: 'https://i.ibb.co/HVB4s69/youtube-social-icon-red.png',
         handleClick:() => {
             window.location.assign('https://youtube.com/c/@Ignorance');
-        }
+        },
+        handleError:() => {
+            this.src='./img/youtube-social-icon-red.png'
+        },
     },
     Osu: {
         id: 'Osu',
         src: 'https://i.ibb.co/dppVC00/full-color-osu-logo.png',
         handleClick:() => {
             window.location.assign('https://osu.ppy.sh/users/20178628')
-        }
+        },
+        handleError:() => {
+            this.src='./img/full-color-osu-logo.png'
+        },
     },
     Twitter: {
         id: 'Twitter',
         src: 'https://i.ibb.co/DW5yWqs/twitter-logo-white.png',
         handleClick:() => {
             window.location.assign('https://twitter.com/ignorancespam');
-        }
+        },
+        handleError:() => {
+            this.src='./img/twitter-logo-white.png'
+        },
     },
     Twitch: {
         id: 'Twitch',
         src: 'https://i.ibb.co/KjcZxXC/Twitch-Glitch-Purple.png',
         handleClick:() => {
             window.location.assign('https://twitch.tv/strxwberryice')
-        }
+        },
+        handleError:() => {
+            this.src='./img/Twitch-Glitch-Purple.png'
+        },
     },
 }
